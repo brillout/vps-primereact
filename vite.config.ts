9dc650bbddeac3ef78ece36596fc30f478fa238e
Romuald Brillout <git@brillout.com>
2023-07-20 12:25:11 +0200

    workaround

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,15 @@ import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin'
 import { UserConfig } from 'vite'
 
+const noExternal = process.env.NODE_ENV !== 'production' ? [] : ['primereact']
 const config: UserConfig = {
   plugins: [
     react(),
     ssr({
       prerender: true
     })
-  ]
+  ],
+  ssr: { noExternal }
 }
 
 export default config
