2f7cabb07d4fa641103b51b6da333a7300826170
f.batti <fabio.batti93@gmail.com>
2023-07-20 11:27:55 +0200

    first push

---
+++ pages/index.page.tsx
@@ -0,0 +1,12 @@
+export { Page };
+// import { ProgressSpinner } from 'primereact/progressspinner';
+import { Badge } from 'primereact/badge';
+
+function Page() {
+    return (
+        <>
+            <Badge />
+            {/* <ProgressSpinner /> */}
+        </>
+    );
+}
